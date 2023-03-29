fa4b3711801ad91efeb46be840971ac757b1c333
Zehra Bekar <zehrabekar54@gmail.com>
2023-03-29 14:50:51 +0300

    entry

---
+++ src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello yeni sınıf");
+    }
+}
+// psvm + enter = public static void main
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("java öğreniyorum");
+        System.out.print("zehra bekar");
+        System.out.print("java dersleri \n"); // üst satırla yanyana yazdı. fakat sondaki \n karakteri sebebiyle imleci alt satıra aldı
+        System.out.println("ilk projemiz");
     }
 }
+
+/*
+çıktı :
+java öğreniyorum
+zehra bekarjava dersleri
+ilk projemiz */
+
+
+/*
+Sınıf isimlendirmeleri :
+-büyük harfle başlanır
+-iki kelimeden oluşuyorsa birleşik ve ilk harfleri büyük yazılır. örnek : HelloWorld , IlkSinif, OgrenciDetay
+-türkçe karakter kullanılmaz .
+
+*Bir sınıfın çalışabilmesi için main isimli metota sahip olmalıdır, burası uygulamanın başlangıç noktasıdır.
+*Birden fazla main metotu olabilir.
+*statement : bir aksiyonu veya aksiyonlar sırasını belirtir.merhaba dünya ifadesinin gösterilmesine statement denir.her bir statement ; ile sonlanır.
+*javada bazı kelimeler reserved words olrak geçer. Bu kelimelerin java için anlamı vardır . o yüzden değişken , metot , sınıf vs gibi şeyler oluştururken bu isimleri
+kullanmamalıyız . örnek : class , static, public, void ...
+hatalı kullanım örneği (değişken) : int void = 10 -> yapılamaz
+*süslü parantezlerden sonra ; kullanılmaz
+*sout + tab =  System.out.println();
+* System.out.println(); -> imleci bir alt satıra alır
+ System.out.print(); imleci aynı satırda bırakır
+
+
+
+
+*/
