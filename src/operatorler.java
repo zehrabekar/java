c145b51c8fe3ee8c4b13cb77939815d4358cd70a
Zehra Bekar <zehrabekar54@gmail.com>
2023-04-03 14:55:51 +0300

    note about operators

---
+++ src/operatorler.java
@@ -0,0 +1,19 @@
+public class operatorler {
+    public static void main(String[] args) {
+        /* mantıksal operatörler :
+        ! = true değeri false , false değeri true yapar
+         deger1 = false;
+        !deger1 = true  */
+
+
+        /*işlem önceliği :
+        -parantez içi
+        - ++ ve -- (eğer değişkenden önce ise) (--sayi)
+        - çarpma ve bölme
+        -toplama ve çıkarma
+        - = atama işlemi
+        - ++ ve -- (eğer değişkenden sonra ise) (sayi++)
+         */
+
+    }
+}
